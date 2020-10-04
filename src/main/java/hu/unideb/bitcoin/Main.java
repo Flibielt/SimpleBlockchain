92750210e9e23cbe92cf60269ae1b0d371c386d3
Csaba Hermann <hcsabi98@gmail.com>
2020-10-04 17:27:43 +0200

    Created test run

--- src/main/java/hu/unideb/bitcoin/Main.java
+++ src/main/java/hu/unideb/bitcoin/Main.java
@@ -5,7 +5,13 @@ public class Main {
     public static void main(String[] args) {
         Blockchain blockchain = new Blockchain();
 
-        blockchain.addTransaction("asd", "asd");
+        blockchain.addTransaction("sender1", "message1");
+        blockchain.addTransaction("sender2", "message2");
+
+        blockchain.addBlock();
+
+        blockchain.addTransaction("sender3", "message3");
+        blockchain.addTransaction("sender4", "message4");
     }
 
 }
